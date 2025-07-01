fd0596433066b35cc6b3109e946b4bfe411f6279
Crabii <baptiste.julienne@outlook.com>
2025-07-01 11:00:21 +0200

    feat(controller_entity): ajout des entités et des controllers

--- src/Repository/HabiteRepository.php
+++ src/Repository/HabiteRepository.php
@@ -2,18 +2,18 @@
 
 namespace App\Repository;
 
-use App\Entity\Abite;
+use App\Entity\Habite;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Abite>
+ * @extends ServiceEntityRepository<Habite>
  */
 class HabiteRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Abite::class);
+        parent::__construct($registry, Habite::class);
     }
 
     //    /**
